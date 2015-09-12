574bef23f1ff612663ca1eb662e96034a15747b9
galoperidol <dvgrishechkin@yandex.ru>
2015-09-12 10:06:24 +0300

    1. Added: Flickr "get size" answer parser and it tests.

--- app/src/main/scala/ru/triton265/flickrfirstimagescala/app/FlickrClient.scala
+++ app/src/main/scala/ru/triton265/flickrfirstimagescala/app/FlickrClient.scala
@@ -27,29 +27,54 @@ object FlickrClient {
 
     val result = response.body.decodeOption[FlickrClient.SearchResult]
     for {
-      r <- result if r.stat == "ok"
+      r <- result if "ok" == r.stat
       photos = r.photos if photos.total > 0 && photos.photo.nonEmpty
     } yield photos.photo.head.id
   }
 
+  def getSizes(id: Option[String]) : Option[List[Size]] = {
+    return None
+  }
+
   // For Argonaut.
-  // Parse search result
+  // Parse search result.
   case class Photo(id: String)
+  case class Photos(total: Long, photo: List[Photo])
+  case class SearchResult(stat: String, photos: Photos)
+
   object Photo {
     implicit def PhotoCodecJson: CodecJson[Photo]
       = casecodec1(Photo.apply, Photo.unapply)("id")
   }
 
-  case class Photos(total: Long, photo: List[Photo])
   object Photos {
     implicit def PhotosCodecJson: CodecJson[Photos]
       = casecodec2(Photos.apply, Photos.unapply)("total", "photo")
   }
 
-  case class SearchResult(stat: String, photos: Photos)
   object SearchResult {
     implicit def SearchResultCodecJson: CodecJson[SearchResult]
       = casecodec2(SearchResult.apply, SearchResult.unapply)("stat", "photos")
+  }
+
+  // Parse get sizes reuslt.
+  case class Size(label: String, width: Long, height: Long, source: String)
+  case class Sizes(size: List[Size])
+  case class GetSizesResult(stat: String, sizes: Sizes)
+
+  object Size {
+    implicit def SizeCodecJson: CodecJson[Size]
+      = casecodec4(Size.apply, Size.unapply)("label", "width", "height", "source")
+  }
+
+  object Sizes {
+    implicit def SizesCodecJson: CodecJson[Sizes]
+      = casecodec1(Sizes.apply, Sizes.unapply)("size")
+  }
+
+  object GetSizesResult {
+    implicit def GetSizesResultCodecJson: CodecJson[GetSizesResult]
+      = casecodec2(GetSizesResult.apply, GetSizesResult.unapply)("stat", "sizes")
   }
 }
 
--- app/src/test/scala/ru/triton265/flickrfirstimagescala/app/FlickrClientTest.scala
+++ app/src/test/scala/ru/triton265/flickrfirstimagescala/app/FlickrClientTest.scala
@@ -27,7 +27,7 @@ class FlickrClientTest {
     */
 
     val id2 = for {
-      r <- result if r.stat == "ok"
+      r <- result if "ok" == r.stat
       photos = r.photos if photos.total > 0 && photos.photo.nonEmpty
     } yield photos.photo.head.id
 
@@ -38,17 +38,8 @@ class FlickrClientTest {
     val input = "wrong json string"
     val result = input.decodeOption[FlickrClient.SearchResult]
 
-    /*
-    val id = result
-      .filter(r => "ok" == r.stat)
-      .map(r => r.photos)
-      .filter(p => p.total > 0 && p.photo.nonEmpty)
-      .map(p => p.photo.head.id)
-    assertTrue(id.isEmpty)
-    */
-
     val id2 = for {
-      r <- result if r.stat == "ok"
+      r <- result if "ok" == r.stat
       photos = r.photos if photos.total > 0 && photos.photo.nonEmpty
     } yield photos.photo.head.id
 
@@ -58,5 +49,78 @@ class FlickrClientTest {
   @Test def testSearch() {
     val id = FlickrClient.searchFirst(Some("nice butt"))
     assertFalse(id.isEmpty)
+  }
+
+  @Test def testNormalGetSizesParse() {
+    val input =
+      """
+        |{ "sizes": { "canblog": 0, "canprint": 0, "candownload": 1,
+        |    "size": [
+        |      { "label": "Square", "width": 75, "height": 75, "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_s.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/sq\/", "media": "photo" },
+        |      { "label": "Large Square", "width": "150", "height": "150", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_q.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/q\/", "media": "photo" },
+        |      { "label": "Thumbnail", "width": 80, "height": 100, "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_t.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/t\/", "media": "photo" },
+        |      { "label": "Small", "width": "192", "height": "240", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_m.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/s\/", "media": "photo" },
+        |      { "label": "Small 320", "width": "256", "height": "320", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_n.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/n\/", "media": "photo" },
+        |      { "label": "Medium", "width": "400", "height": "500", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/m\/", "media": "photo" },
+        |      { "label": "Medium 640", "width": "512", "height": "640", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_z.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/z\/", "media": "photo" },
+        |      { "label": "Medium 800", "width": "640", "height": "800", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_c.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/c\/", "media": "photo" },
+        |      { "label": "Large", "width": "819", "height": "1024", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_b.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/l\/", "media": "photo" },
+        |      { "label": "Original", "width": "1080", "height": "1350", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_ba7cdae1fe_o.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/o\/", "media": "photo" }
+        |    ] }, "stat": "ok" }
+      """.stripMargin
+
+    val result = input.decodeOption[FlickrClient.GetSizesResult]
+    assertFalse(result.isEmpty)
+
+    val sizeList = result
+      .filter(p => "ok" == p.stat)
+      .map(p => p.sizes.size)
+
+    assertFalse(sizeList.isEmpty)
+
+    val source = for {
+      list <- sizeList
+    } yield list.filter("Medium" == _.label).head.source
+
+    assertFalse(source.isEmpty)
+  }
+
+  @Test def testNoMediumGetSizesParse() {
+    val input =
+      """
+        |{ "sizes": { "canblog": 0, "canprint": 0, "candownload": 1,
+        |    "size": [
+        |      { "label": "Square", "width": 75, "height": 75, "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_s.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/sq\/", "media": "photo" },
+        |      { "label": "Large Square", "width": "150", "height": "150", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_q.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/q\/", "media": "photo" },
+        |      { "label": "Thumbnail", "width": 80, "height": 100, "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_t.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/t\/", "media": "photo" },
+        |      { "label": "Small", "width": "192", "height": "240", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_m.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/s\/", "media": "photo" },
+        |      { "label": "Small 320", "width": "256", "height": "320", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_n.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/n\/", "media": "photo" },
+        |      { "label": "Large", "width": "819", "height": "1024", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_701de65407_b.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/l\/", "media": "photo" },
+        |      { "label": "Original", "width": "1080", "height": "1350", "source": "https:\/\/farm1.staticflickr.com\/753\/21127436650_ba7cdae1fe_o.jpg", "url": "https:\/\/www.flickr.com\/photos\/133459415@N05\/21127436650\/sizes\/o\/", "media": "photo" }
+        |    ] }, "stat": "ok" }
+      """.stripMargin
+
+    val result = input.decodeOption[FlickrClient.GetSizesResult]
+    assertFalse(result.isEmpty)
+
+    val sizeList = result
+      .filter(p => "ok" == p.stat)
+      .map(p => p.sizes.size)
+
+    assertFalse(sizeList.isEmpty)
+
+    val source = for {
+      list <- sizeList
+      medium = list.filter("Medium" == _.label) if !medium.isEmpty
+    } yield medium.head.source
+
+    assertTrue(source.isEmpty)
+  }
+
+  @Test def testWrongGetSizesParse() {
+    val input = "Wrong json string"
+
+    val result = input.decodeOption[FlickrClient.GetSizesResult]
+    assertTrue(result.isEmpty)
   }
 }
